1bfe6e7e355219608d8a26090694a386e6d180c9
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2024-02-18 20:09:37 +0300

    Create NodeExporter-auto-install.sh

---
+++ NodeExporter-auto-install.sh
@@ -0,0 +1,41 @@
+# обновляемся
+sudo apt-get update && sudo apt-get upgrade -y
+# скачиваем node_exporter, разархивируем, прописываем права, удаляем лищнее
+cd $HOME && \
+wget https://github.com/prometheus/node_exporter/releases/download/v1.2.0/node_exporter-1.2.0.linux-amd64.tar.gz
+tar xvf node_exporter-1.2.0.linux-amd64.tar.gz
+rm node_exporter-1.2.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.2.0.linux-amd64 node_exporter
+chmod +x $HOME/node_exporter/node_exporter
+mv $HOME/node_exporter/node_exporter /usr/bin
+rm -Rvf $HOME/node_exporter/
+# создаём файл сервиса exporterd
+sudo tee /etc/systemd/system/exporterd.service > /dev/null <<EOF
+[Unit]
+Description=node_exporter
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=/usr/bin/node_exporter
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+# запускаем сервис exporterd
+sudo systemctl daemon-reload
+sudo systemctl enable exporterd
+sudo systemctl restart exporterd
+# наш логотип для красоты
+echo -e "\033[1;36m"
+echo -e "████╗░██████╗░███████╗████╗  ░█████╗░███████╗░█████╗░███████╗██╗░░░░░░█████╗░"
+echo -e "██╔═╝██╔════╝░╚════██║╚═██║  ██╔══██╗╚════██║██╔══██╗╚════██║██║░░░░░██╔══██╗"
+echo -e "██║░░██║░░██╗░░░░░██╔╝░░██║  ███████║░░███╔═╝███████║░░███╔═╝██║░░░░░██║░░██║"
+echo -e "██║░░██║░░╚██╗░░░██╔╝░░░██║  ██╔══██║██╔══╝░░██╔══██║██╔══╝░░██║░░░░░██║░░██║"
+echo -e "████╗╚██████╔╝░░██╔╝░░████║  ██║░░██║███████╗██║░░██║███████╗███████╗╚█████╔╝"
+echo -e "╚═══╝░╚═════╝░░░╚═╝░░░╚═══╝  ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚══════╝╚══════╝░╚════╝░"
+echo -e "\033[1;34m"
+echo
+echo -e "\033[1;32mTelegram community: \033[5;31mhttps://t.me/g7monitor\033[0m"
+echo -e "\033[0m"
